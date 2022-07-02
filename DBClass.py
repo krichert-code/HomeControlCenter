644c16d726638ce4e7d62d539ca8488a753592a0
root <root@dom.ovh>
2022-07-02 14:06:31 +0200

    Added action program class and prev_energy field to database

--- DBClass.py
+++ DBClass.py
@@ -3,40 +3,43 @@
 import sqlalchemy as db
 import os.path
 
-
 class DBClass(object):
 
     def __init__(self):
-        engine = db.create_engine('sqlite:///hcc.db')
-        connection = engine.connect()
-
         if os.path.isfile('hcc.db') == False:
+            engine = db.create_engine('sqlite:///hcc.db')
+            connection = engine.connect()
+
             metadata = db.MetaData()
 
             energy = db.Table('EnergyData', metadata, db.Column('Id',
                               db.Integer()), db.Column('month_name',
                               db.String(255), nullable=False),
                               db.Column('energy', db.Integer(),
-                              default=0))
+                              default=0),
+                              db.Column('prev_energy', db.Integer()))
 
             metadata.create_all(engine)  # Creates the table
 
             query = db.insert(energy)
             values_list = [
-                {'Id': '1', 'month_name': 'Styczen', 'energy': 0},
-                {'Id': '2', 'month_name': 'Luty', 'energy': 0},
-                {'Id': '3', 'month_name': 'Marzec', 'energy': 0},
-                {'Id': '4', 'month_name': 'Kwiecien', 'energy': 0},
-                {'Id': '5', 'month_name': 'Maj', 'energy': 0},
-                {'Id': '6', 'month_name': 'Czerwiec', 'energy': 0},
-                {'Id': '7', 'month_name': 'Lipiec', 'energy': 0},
-                {'Id': '8', 'month_name': 'Sierpien', 'energy': 0},
-                {'Id': '9', 'month_name': 'Wrzesien', 'energy': 0},
-                {'Id': '10', 'month_name': 'Pazdziernik', 'energy': 0},
-                {'Id': '11', 'month_name': 'Listopad', 'energy': 0},
-                {'Id': '12', 'month_name': 'Grudzien', 'energy': 0},
+                {'Id': '1', 'month_name': 'Styczen', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '2', 'month_name': 'Luty', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '3', 'month_name': 'Marzec', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '4', 'month_name': 'Kwiecien', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '5', 'month_name': 'Maj', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '6', 'month_name': 'Czerwiec', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '7', 'month_name': 'Lipiec', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '8', 'month_name': 'Sierpien', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '9', 'month_name': 'Wrzesien', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '10', 'month_name': 'Pazdziernik', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '11', 'month_name': 'Listopad', 'energy': 0, 'prev_energy' : 0},
+                {'Id': '12', 'month_name': 'Grudzien', 'energy': 0, 'prev_energy' : 0},
                 ]
             ResultProxy = connection.execute(query, values_list)
+        else:
+            engine = db.create_engine('sqlite:///hcc.db')
+            connection = engine.connect()
 
     def updateEnergy(self, monthId, value):
         engine = db.create_engine('sqlite:///hcc.db')
